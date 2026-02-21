70071bcd787c85a8eae19bb734657142855cb40c
league <maddoxseul@sbsd.net>
2026-02-20 18:13:17 -0800

    hey BABEEE

--- src/_05_cow_timer/CowTimer.java
+++ src/_05_cow_timer/CowTimer.java
@@ -2,43 +2,32 @@ package _05_cow_timer;
 
 import game_tools.Sound;
 
-/*
- * Objective: Create a Cow Count Down program!
- * 
- * In this class:
- * 1. Make a constructor for the CowTimer class that initializes the seconds
- *    variable.
- * 
- * 2. In the start() method, count down the seconds, print the current second,
- *    then sleep for the number of seconds using Thread.sleep(int milliseconds).
- *    For example, if the count down seconds is 10, the console should print
- *    the remaining seconds with a 1 second delay between each print:
- *    10
- *    9
- *    8
- *    ...
- * 
- * 3. After the timer is finished, use the playSound() method to play a moo
- *    sound. You can use "moo.wav" as the sound file.
- * 
- * In the CowTimerRunner class:
- * Complete the main method
- */
 public class CowTimer {
-    private int seconds;
+	private int seconds;
 
-    public void setTime(int seconds) {
-        this.seconds = seconds;
-        System.out.println("Cow time set to " + this.seconds + " seconds.");
-    }
+	public CowTimer(int seconds) {
+		setTime(seconds);
 
-    public void start() throws InterruptedException {
-        
-    }
+	}
 
-    public void playSound(final String file) {
-        String fileName = "_05_cow_timer/" + file;
-        Sound sound = new Sound(fileName);
-        sound.play();
-    }
+	public void setTime(int seconds) {
+		this.seconds = seconds;
+		System.out.println("Cow time set to " + this.seconds + " seconds.");
+	}
+
+	public void start() throws InterruptedException {
+		for (int x = seconds; x > -1; x -= 1) {
+			System.out.println(x);
+			Thread.sleep(1000);
+		}
+
+		playSound("moo.wav");
+
+	}
+
+	public void playSound(final String file) {
+		String fileName = "_05_cow_timer/" + file;
+		Sound sound = new Sound(fileName);
+		sound.play();
+	}
 }
