93c5d100b30cd04db6a9f8ad17240878d564cdb7
league <league@192.168.1.135>
2026-03-13 18:28:53 -0700

    uwu

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -3,130 +3,130 @@ package _06_frogger;
 import processing.core.PApplet;
 
 public class Frogger extends PApplet {
-    static final int WIDTH = 800;
-    static final int HEIGHT = 600;
-    int frogX = 400;
-    int frogY = 575;
-    int hopDistance;
-    Car car_01;
-    Car car_02;
-    
-    @Override
-    public void settings() {
-        size(WIDTH, HEIGHT);
+	static final int WIDTH = 800;
+	static final int HEIGHT = 600;
+	int frogX = 400;
+	int frogY = 575;
+	int hopDistance;
+	Car car_01;
+	Car car_02;
 
-    }
+	@Override
+	public void settings() {
+		size(WIDTH, HEIGHT);
+	}
 
-    @Override
-    public void setup() {
-    	
-    	car_01 = new Car(500, 273, 56, 20);
-    	car_02 = new Car(400, 273, 56, 20);
-    	
-    	
-    }
+	@Override
+	public void setup() {
 
-    @Override
-    public void draw() {
-    	background(237, 166, 42);
-    	fill(23, 246, 155);
-    	ellipse(frogX, frogY, 50, 40);
-    	car_01.display();
-    	car_02.display();
-    	car_01.moveLeft();
-    	car_02.moveRight();
-    	
-    }
-    
-    void stopThatYouIdioticFrog() {
-    	if (frogX < 0) {
-    		frogX = 0;
-    	
-    	}
-    	if (frogY < 0) {
-    		frogY = 0;
-    	
-    	}
-    	if (frogX > WIDTH) {
-    		frogX = WIDTH;
-    	
-    	}
-    	if (frogY > HEIGHT) {
-    		frogY = HEIGHT;
-    	
-    	}
-    	
-    }
-    
-    public void keyPressed()
-    {
-        if(key == CODED){
-            if(keyCode == UP)
-            {
-            	frogY -= 10;
-            }
-            else if(keyCode == DOWN)
-            {
-            	frogY += 10;
-            }
-            else if(keyCode == RIGHT)
-            {
-            	frogX += 10;
-            }
-            else if(keyCode == LEFT)
-            {
-            	frogX -= 10;
-            }
-            stopThatYouIdioticFrog();
-        }
-    }
-    
-    class Car {
-    	int carX;
-    	int carY;
-    	int carSize;
-    	int carSpeed;
-    	
+		car_01 = new Car(500, 273, 56, 20);
+		car_02 = new Car(400, 273, 56, 20);
+	}
 
-//constructor for Car class that initializes each member variable with parameters
-    	public Car(int carX, int carY, int carSize, int carSpeed) {
-    		this.carX = carX;
-    		this.carY = carY;
-    		this.carSize = carSize;
-    		this.carSpeed = carSpeed;
-    		
+	@Override
+	public void draw() {
+		background(237, 166, 42);
+		fill(23, 246, 155);
+		ellipse(frogX, frogY, 50, 40);
+		car_01.display();
+		car_02.display();
+		car_01.moveLeft();
+		car_02.moveRight();
+		if (car_01.intersects(car_01) == true) {
+			
+		}
+		if (car_02.intersects(car_02) == true) {
+			
+		}
 
-    	}
-    	
-    	void moveLeft() {
-    		carX -= carSpeed;
-    		if (carX < -carSize) {
-    			carX = WIDTH;
-    			
-    		}
-    		
-    	}
-    	
-    	void moveRight() {
-    		carX += carSpeed;
-    		if (carX > WIDTH) {
-    			carX = -carSize;
-    			
-    		}
-    		
-    	}
+	}
 
+	void stopThatYouIdioticFrog() {
+		if (frogX < 0) {
+			frogX = 0;
+		}
+		if (frogY < 0) {
+			frogY = 0;
+		}
+		if (frogX > WIDTH) {
+			frogX = WIDTH;
+		}
+		if (frogY > HEIGHT) {
+			frogY = HEIGHT;
+		}
+	}
 
-void display()
-  {
-    fill(0,255,0);
-    rect(carX, carY, carSize, 50);
+	public void keyPressed() {
+		if (key == CODED) {
+			if (keyCode == UP) {
+				frogY -= 10;
+			} else if (keyCode == DOWN) {
+				frogY += 10;
+			} else if (keyCode == RIGHT) {
+				frogX += 10;
+			} else if (keyCode == LEFT) {
+				frogX -= 10;
+			}
+			stopThatYouIdioticFrog();
+		}
+	}
 
-    	
-    }
+	class Car {
+		int carX;
+		int carY;
+		int carSize;
+		int carSpeed;
 
-    }
-    static public void main(String[] args) {
-        PApplet.main(Frogger.class.getName());
-    }
+		public Car(int carX, int carY, int carSize, int carSpeed) {
+			this.carX = carX;
+			this.carY = carY;
+			this.carSize = carSize;
+			this.carSpeed = carSpeed;
+		}
+
+		int getX() {
+			return carX;
+		}
+
+		int getY() {
+			return carY;
+		}
+
+		int getSize() {
+			return carSize;
+		}
+
+		void moveLeft() {
+			carX -= carSpeed;
+			if (carX < -carSize) {
+				carX = WIDTH;
+			}
+		}
+
+		void moveRight() {
+			carX += carSpeed;
+			if (carX > WIDTH) {
+				carX = -carSize;
+			}
+		}
+
+		void display() {
+			fill(0, 255, 0);
+			rect(carX, carY, carSize, 50);
+		}
+
+		boolean intersects(Car car) {
+			if ((frogY > car.getY() && frogY < car.getY() + 50)
+					&& (frogX > car.getX() && frogX < car.getX() + car.getSize())) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+	}
+
+	static public void main(String[] args) {
+		PApplet.main(Frogger.class.getName());
+	}
 }
